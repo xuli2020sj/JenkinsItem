436f050cd74ecd1b69d8235da5a64517163c3302
li.xu <xuli2020sj@gmail.com>
2023-03-16 17:51:57 +0800

    submit pdc file

---
+++ fpga_pdc/scp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pexpect
+import sys 
+import time
+import os
+
+prt = "Password:"
+cmdBase = "scp %s svc.fpgatest@fpga05.cixcomputing.cn:%s" % (sys.argv[1], sys.argv[2])
+mkcmd = "\"mkdir -p %s\"" % os.path.dirname(sys.argv[2])
+
+# mkdir
+cid = pexpect.spawn("ssh svc.fpgatest@fpga05.cixcomputing.cn " + mkcmd , encoding='utf-8', codec_errors='replace')
+cid.logfile = sys.stdout
+cid.expect(prt)
+# send password
+cid.sendline("Cix@88008080")
+cid.expect([pexpect.EOF, pexpect.TIMEOUT])
+
+cid = pexpect.spawn(cmdBase, encoding='utf-8', codec_errors='replace')
+cid.logfile = sys.stdout
+cid.expect(prt)
+# send password
+cid.sendline("Cix@88008080")
+cid.expect(['\$', pexpect.EOF], timeout=60)
+
---
+++ fpga_pdc/swflash.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+"""
+1. open and ssh sessions 
+2. do operations
+args:
+arg1 - The fpga PC name like fpga01 or fpga01-sub1
+arg2 - The uploaded flash fw file. It is omissible when default fw is OK.
+arg3 - Whether to update the default fw using the uploaded FW.
+"""
+from pexpect import pxssh
+import pexpect
+import sys
+import os
+import time
+
+# global 
+pcInfo = {
+    "fpga04": {'name': 'svc.fpgatest', 'ip': 'fpga04.cixcomputing.cn', 'passwd': 'Cix@88008080'},
+    "fpga05": {'name': 'svc.fpgatest', 'ip': 'fpga05.cixcomputing.cn', 'passwd': 'Cix@88008080'},
+    "fpga06": {'name': 'svc.fpgatest', 'ip': 'fpga06.cixcomputing.cn', 'passwd': 'Cix@88008080'},
+}
+cPath = os.path.split(os.path.realpath(__file__))[0] + "/"
+
+
+# ssh login function
+def pSSH(pinfo):
+    chdPid = pxssh.pxssh(encoding='utf-8', timeout=5, codec_errors='replace')
+    # only save output
+    chdPid.logfile = sys.stdout
+    chdPid.login(pinfo['ip'], pinfo['name'], pinfo['passwd'])
+    return chdPid
+
+
+def heid(eid):
+    if eid == 0 or eid == 1:
+        return 1
+    else:
+        return 0
+
+
+# ssh basic operations
+def bOP(ss, oplist):
+    op, ep, to = oplist
+    ss.sendline(op)
+    eid = ss.expect([pexpect.EOF, pexpect.TIMEOUT] + [ep], timeout=to)
+    return heid(eid)
+
+
+if __name__ == "__main__":
+    # get target PC arg
+    tPC = sys.argv[1].split('-')[0]
+    # default fw path in remote PC
+    dfwPath = "/home/svc.fpgatest/devops/lab_loader/%s/dfw/cix_flash_all.bin" % (tPC)
+    # temp path for uploader fw
+    tfwPath = "/home/svc.fpgatest/devops/lab_loader/%s/tfw/cix_flash_all.bin" % (tPC)
+    pcSSH = pSSH(pcInfo[tPC])
+    ffwPath = dfwPath
+    # whether to update fw
+    if len(sys.argv) >= 3 and sys.argv[2] != 'null':
+        os.system(cPath + "scp.py " + sys.argv[2] + " " + tfwPath)
+        ffwPath = tfwPath
+    if len(sys.argv) >= 4 and sys.argv[2] != 'null' and sys.argv[3] == "Yes":
+        os.system(cPath + "scp.py " + sys.argv[2] + " " + dfwPath)
+
+    # try to flash the FW
+    flList = ["pdc_linux_console -i " + ffwPath, "Flashing PASS", 120]
+    rv = bOP(pcSSH, flList)
+    sys.exit(rv)
