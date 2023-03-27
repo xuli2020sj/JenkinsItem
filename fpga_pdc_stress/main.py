fb336eee86cdc438322d3a9a2827d726fd2d466a
li.xu <xuli2020sj@gmail.com>
2023-03-27 12:51:47 +0800

    update

---
+++ fpga_pdc_stress/main.py
@@ -0,0 +1,29 @@
+import argparse
+import signal
+import pytest
+import global_val
+import logging
+import logging.config
+
+
+def quit(signum, frame):
+    logging.info("Test Stopping！")
+    global_val.set_value('stopFlag', True)
+    # pytest.exit("Test Stopping！")
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, quit)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--count', '-c', required=False, type=int, default=10)
+    parser.add_argument('--module', '-m', required=False, type=str, default="test_")
+
+    args = parser.parse_args()
+    count_cmd = "--count={}".format(args.count)
+    test_cmd = "test_stability.py::{}".format(args.module)
+
+    global_val.init()
+    global_val.set_value('stopFlag', False)
+
+    pytest.main(["-s", test_cmd, count_cmd])
---
+++ fpga_pdc_stress/test_stability.py
@@ -0,0 +1,31 @@
+import logging
+import re
+import subprocess
+from time import sleep
+import global_val
+import pytest
+
+
+@pytest.mark.repeat(500)
+def test_flashing():
+    p = subprocess.Popen(r'~/bin/pdc_linux_console -i ~/bin/cix_flash_all.bin',
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         shell=True
+                         )
+    logging.info('start flashing')
+    p.wait()
+    logging.info('flashing end')
+    out = p.stdout.read()
+    logging.info(out.decode("utf8"))
+    pattern = re.compile('OK')
+    result = re.search(pattern, out.decode("utf8"))
+    assert result is not None
+
+def test_():
+    stopFlag = global_val.get_value('stopFlag')
+    if stopFlag:
+        pytest.exit("Program stop!")
+    logging.info('start flashing {}')
+    sleep(1)
+    logging.info('flashing end {}')
